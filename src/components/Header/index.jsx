4e6ea0d8f0f4694c4b6ed9cb6e8c402153726c92
lcsvinhas <l.vinhas.lv@gmail.com>
2025-06-13 16:10:12 -0300

    criei responsividade do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,18 +2,26 @@ import { Link } from "react-router-dom";
 import * as styles from "./Header.module.css";
 import logoBranca from "../../img/logo-branca-1.png";
 import vector from "../../img/vector.png";
+import { useState } from "react";
 
 export default function Header() {
+    const [menuAberto, setMenuAberto] = useState(false);
+
+    const toggleMenu = () => setMenuAberto(!menuAberto);
 
     return (
         <header className={styles.header}>
             <Link to={"/"}><img src={logoBranca} alt="Logotipo do sebo virtual Serrabook" /></Link>
+
+            <button onClick={toggleMenu} className={styles.menuBtn}>&#9776;</button>
+
             <div className={styles.contato}>
                 <img src={vector} alt="Contorno de um ser humano, ilustrando um perfil" />
                 <h2>Bem vindo, usuário</h2>
             </div>
-            <nav>
-                <ul className={styles.ul}>
+
+            <nav className={`${styles.menu} ${menuAberto ? styles.ativo : ""}`}>
+                <ul>
                     <li><Link className={styles.elemento} to={"/produtos"}>Produtos</Link></li>
                     <li><Link className={styles.elemento} to={"/login"}>Login</Link></li>
                     <li><Link className={styles.elemento} to={"/atualizar"}>Atualizar</Link></li>
@@ -21,5 +29,5 @@ export default function Header() {
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
