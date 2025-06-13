a9f980d511f6596a3115fdf5671c78551494694f
lcsvinhas <l.vinhas.lv@gmail.com>
2025-06-13 15:20:16 -0300

    criei header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
 import * as styles from "./Header.module.css";
+import logoBranca from "../../img/logo-branca-1.png";
+import vector from "../../img/vector.png";
 
 export default function Header() {
+
     return (
         <header className={styles.header}>
-            <h1>Serrabook</h1>
+            <Link to={"/"}><img src={logoBranca} alt="Logotipo do sebo virtual Serrabook" /></Link>
+            <div className={styles.contato}>
+                <img src={vector} alt="Contorno de um ser humano, ilustrando um perfil" />
+                <h2>Bem vindo, usuário</h2>
+            </div>
             <nav>
                 <ul className={styles.ul}>
-                    <li><Link className={styles.elemento} to={"/"}>Home</Link></li>
                     <li><Link className={styles.elemento} to={"/produtos"}>Produtos</Link></li>
                     <li><Link className={styles.elemento} to={"/login"}>Login</Link></li>
                     <li><Link className={styles.elemento} to={"/atualizar"}>Atualizar</Link></li>
+                    <li><button>Dark Mode</button></li>
                 </ul>
             </nav>
         </header>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,18 @@
+import banner from "../../img/banner-1.png";
+import * as styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
-        <h2>Home</h2>
+      <img className={styles.banner} src={banner} alt="Banner sobre o Serrabook Day. Possui três livros de programação em pé e em perspectiva." />
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ipsa, cupiditate voluptas officia accusamus esse placeat commodi, quae explicabo temporibus laborum, similique perspiciatis? Voluptatum culpa nam, voluptatibus repudiandae officia iusto?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ipsa, cupiditate voluptas officia accusamus esse placeat commodi, quae explicabo temporibus laborum, similique perspiciatis? Voluptatum culpa nam, voluptatibus repudiandae officia iusto?</p>
+      </div>
+      <div className={styles.verMais}>
+        <Link to={"/produtos"}><button>Ver mais</button></Link>
+      </div>
     </div>
   )
 }
