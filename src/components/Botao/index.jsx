7bd5fac332c034f5e6e76e925613b608598cd736
ThaynaL <cristinathayna06@gmail.com>
2025-06-14 15:10:09 -0300

    Finalizando html e css do Atualizar

--- src/components/Botao/index.jsx
+++ src/components/Botao/index.jsx
@@ -4,17 +4,19 @@ import * as styles from "./Botao.module.css";
 export default function Botao({linkPagina, imagem}) {
     return (
         <div className={styles.btnIcon}>
-            <Link to={linkPagina}>
+            {/* <Link to={linkPagina}> */}
                 <button>
                     <img src={imagem.url} alt={imagem.descricao}/>
                 </button>
-            </Link>
+            {/* </Link> */}
         </div>
     )
 }
 
 
-{/* <Botao
+{/* import Botao from "../../components/Botao";
+    
+    <Botao
                     linkPagina={`/`}
                     imagem={
                         {
--- src/pages/Atualizar/index.jsx
+++ src/pages/Atualizar/index.jsx
@@ -1,10 +1,65 @@
+
 import Header from "../../components/Header";
+import { useEffect, useState } from 'react'
+import * as styles from './Atualizar.module.css'
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import Botao from "../../components/Botao";
+
 
 export default function Atualizar() {
+  const [livro, setLivro] = useState({});
+  const { id } = useParams();
+
+  // useEffect(() => {
+  //   axios
+  //   .get(`http://localhost:8080/`)
+  // })
+
   return (
-    <div>
-      <Header />
-      <h2>Atualizar</h2>
+  <>
+  <Header/>
+    <div className={styles.principal}>
+      <div className={styles.imagem}>
+        <img src="https://ia902309.us.archive.org/view_archive.php?archive=/20/items/l_covers_0008/l_covers_0008_17.zip&file=0008172473-L.jpg" alt="Capa do livro {livro.nome}" />
+      </div>
+
+      <div className={styles.conteudo}>
+        <label htmlFor="titulo">Titulo</label>
+        <input type="text" id="titulo" name="titulo" />
+
+        <label htmlFor="preco">Preço R$</label>
+        <input type="text" id="preco" name="preco" />
+        <div className={styles.botoes}>
+          {/* <Botao
+            imagem={
+              {
+                url: foto,
+                descricao: 'teste'
+              }
+            }
+          />
+          <Botao
+            imagem={
+              {
+                url: foto,
+                descricao: 'teste'
+              }
+            }
+          /> */}
+          <Botao
+            imagem={
+              {
+                url: <MdOutlineDeleteOutline/>,
+                descricao: 'Botão com o icone de lixeira para excluir o o produto'
+              }
+            }
+          />
+        </div>
+      </div>
     </div>
+  </>
+    
   )
 }
