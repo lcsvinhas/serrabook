237953f172f9d5c5c5c6ec9bbf184d8c4e02cdf5
eduardhafecher <eduardha.alves@gmail.com>
2025-06-16 16:06:23 -0300

    Criei tela de cadastro

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,46 +4,66 @@ import logoBranca from "../../img/logo-branca-1.png";
 import vector from "../../img/vector.png";
 import { useState } from "react";
 import { useTheme } from "../../contexts/ThemeContext.jsx";
-import lua from "../../assets/lua.svg"
-import sol from "../../assets/sol.svg"
+import lua from "../../assets/lua.svg";
+import sol from "../../assets/sol.svg";
 
 export default function Header() {
-    const [menuAberto, setMenuAberto] = useState(false);
-    const { darkMode, toggleTheme } = useTheme();
+  const [menuAberto, setMenuAberto] = useState(false);
+  const { darkMode, toggleTheme } = useTheme();
 
-    const toggleMenu = () => setMenuAberto(!menuAberto);
+  const toggleMenu = () => setMenuAberto(!menuAberto);
 
-    return (
-        <header className={styles.header}>
-            <Link to={"/"}><img src={logoBranca} alt="Logotipo do sebo virtual Serrabook" /></Link>
+  return (
+    <header className={styles.header}>
+      <Link to={"/"}>
+        <img src={logoBranca} alt="Logotipo do sebo virtual Serrabook" />
+      </Link>
 
-            <button onClick={toggleMenu} className={styles.menuBtn}>&#9776;</button>
+      <button onClick={toggleMenu} className={styles.menuBtn}>
+        &#9776;
+      </button>
 
-            <div className={styles.contato}>
-                <img src={vector} alt="Contorno de um ser humano, ilustrando um perfil" />
-                <h2>Bem vindo, usuário</h2>
-            </div>
+      <div className={styles.contato}>
+        <img
+          src={vector}
+          alt="Contorno de um ser humano, ilustrando um perfil"
+        />
+        <h2>Bem vindo, usuário</h2>
+      </div>
 
-            <nav className={`${styles.menu} ${menuAberto ? styles.ativo : ""}`}>
-                <ul>
-                    <li><Link className={styles.elemento} to={"/produtos"}>Produtos</Link></li>
-                    <li><Link className={styles.elemento} to={"/login"}>Login</Link></li>
-                    <li><Link className={styles.elemento} to={"/atualizar"}>Atualizar</Link></li>
-                    <li>
-                        <label className={styles.themeSwitch}>
-                            <input
-                                type="checkbox"
-                                checked={darkMode}
-                                onChange={toggleTheme}
-                            />
-                            <span className={styles.slider}>
-                                <img className={styles.iconMoon} src={sol} alt="Modo Escuro" />
-                                <img className={styles.iconSun} src={lua} alt="Modo Claro" />
-                            </span>
-                        </label>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+      <nav className={`${styles.menu} ${menuAberto ? styles.ativo : ""}`}>
+        <ul>
+          <li>
+            <Link className={styles.elemento} to={"/produtos"}>
+              Produtos
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.elemento} to={"/login"}>
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.elemento} to={"/atualizar"}>
+              Atualizar
+            </Link>
+          </li>
+
+          <li>
+            <label className={styles.themeSwitch}>
+              <input
+                type="checkbox"
+                checked={darkMode}
+                onChange={toggleTheme}
+              />
+              <span className={styles.slider}>
+                <img className={styles.iconMoon} src={sol} alt="Modo Escuro" />
+                <img className={styles.iconSun} src={lua} alt="Modo Claro" />
+              </span>
+            </label>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,30 +3,40 @@ import * as styles from "./Login.module.css";
 import entrar from "../../img/entrar.png";
 import vectorsvg from "../../assets/vectorsvg.svg";
 import * as yup from "yup";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const validationPost = yup.object().shape({
-  username: yup.string().email("Email inválido").required("Email é obrigatório"),
-  password: yup.string().min(6, "Senha deve ter pelo menos 6 caracteres").required("Senha é obrigatória"),
+  username: yup
+    .string()
+    .email("Email inválido")
+    .required("Email é obrigatório"),
+  password: yup
+    .string()
+    .min(6, "Senha deve ter pelo menos 6 caracteres")
+    .required("Senha é obrigatória"),
 });
 
 export default function Login() {
-  let navigate = useNavigate()
-  const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(validationPost) })
-  const login = (data => {
+  let navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(validationPost) });
+  const login = (data) => {
     console.log("Dados do login:", data);
-    axios.post("http://localhost:8080/login", data)
+    axios
+      .post("http://localhost:8080/login", data)
       .then((response) => {
         const token = response.headers.get("Authorization");
         localStorage.setItem("token", token);
         navigate("/");
       })
       .catch((error) => console.log("Deu ruim!", error, data));
-  })
-
+  };
 
   return (
     <div>
@@ -35,20 +45,42 @@ export default function Login() {
         <div className={styles.container}>
           <img className={styles.vector} src={vectorsvg} alt="Vector Preto" />
           <form onSubmit={handleSubmit(login)} className={styles.form}>
-            <label htmlFor="username" className={styles.label}>Email</label>
-            <input className={styles.input} type="email" id="username" name="username" {...register("username")} />
-            {errors.username && <span className={styles.error}>{errors.username.message}</span>}
+            <label htmlFor="username" className={styles.label}>
+              Email
+            </label>
+            <input
+              className={styles.input}
+              type="email"
+              id="username"
+              name="username"
+              {...register("username")}
+            />
+            {errors.username && (
+              <span className={styles.error}>{errors.username.message}</span>
+            )}
 
-            <label htmlFor="password" className={styles.label}>Senha</label>
-            <input className={styles.input} type="password" id="password" name="password" {...register("password")} />
-            {errors.password && <span className={styles.error}>{errors.password.message}</span>}
+            <label htmlFor="password" className={styles.label}>
+              Senha
+            </label>
+            <input
+              className={styles.input}
+              type="password"
+              id="password"
+              name="password"
+              {...register("password")}
+            />
+            {errors.password && (
+              <span className={styles.error}>{errors.password.message}</span>
+            )}
 
-            <button className={styles.button} type="submit">Entrar
+            <button className={styles.button} type="submit">
+              Entrar
               <img className={styles.entrar} src={entrar} alt="Entrar" />
             </button>
+            <Link to={"/cadastro"}>Cadastre-se</Link>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
