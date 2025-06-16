ed0023f95fe97554a5d84c1f6cbd1eddcf26c000
ThaynaL <cristinathayna06@gmail.com>
2025-06-16 17:02:32 -0300

    Finalizei pagina de erro

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,10 +1,24 @@
 import Header from "../../components/Header";
+import * as styles from "./Error.module.css";
+import castle from "../../img/castle.png";
+import Botao from "../../components/Botao";
+import { HouseLine } from "phosphor-react";
 
 export default function Error() {
   return (
-    <div>
+    <>
       <Header />
-      <h2>404 - Página não encontrada</h2>
-    </div>
+      <div className={styles.conteudo}>
+        <div className={styles.principal}>
+          <div className={styles.imagem} >
+            <img src={castle} alt="Imagem castelo" />
+          </div>
+          <div className={styles.titulos}>
+            <h1 className={styles.titulo} >Acho que você está muito longe de casa...</h1>
+            <Botao linkPagina="/" icone={<HouseLine color="#021024" />} aria-label="Botão para retornar para o ínicio"></Botao>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
