2935e8e0e3253e4c26b3fcda540b2abd9290895a
lcsvinhas <l.vinhas.lv@gmail.com>
2025-06-16 15:16:10 -0300

    criei footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./styles/global.css"
 import App from './App.jsx'
+import Footer from '../src/components/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Footer />
   </StrictMode>,
 )
